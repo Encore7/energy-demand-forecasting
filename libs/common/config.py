9bf86e726262d4dca5b6fac64ccb94bbfea6407a
Encore7 <amankumar5150@gmail.com>
2025-09-08 17:33:07 +0200

    config file update

--- libs/common/config.py
+++ libs/common/config.py
@@ -2,11 +2,11 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    HOST_IP: str = "host.docker.internal"
+    PROJECT_NAME: str = "energy-forecasting"
+    TZ: str = "Europe/Berlin"
+
     OTEL_EXPORTER_OTLP_ENDPOINT: str = "http://otel-collector:4317"
-    MLFLOW_URL: str = "http://mlflow:5000"
-    REDIS_HOST: str = "redis"
-    REDIS_PORT: int = 6379
-    MINIO_ENDPOINT: str = "http://minio:9000"
 
     class Config:
         env_file = ".env"
